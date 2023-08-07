7bc75c0bcdebe86b6e5febfb8d9f40d84573eade
dbhartia <44170104+dbhartia@users.noreply.github.com>
2023-08-06 20:33:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Driver')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & rocket Smoothie')

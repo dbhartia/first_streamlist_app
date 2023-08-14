5534b2db6d284b2dd45ce78fce97da2c74f9b6d8
dbhartia <44170104+dbhartia@users.noreply.github.com>
2023-08-13 19:57:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the tabke on the page.
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
